99953d98ceb03f3c9c11b0d70f4337ed6a62c828
mecaota <b1014167@fun.ac.jp>
2019-01-24 19:57:18 +0900

    footerのgithubアイコンのクラスをis-darkへ変更

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer">
             <div className="content has-text-centered">
                 <p>当サイトは<a href="https://pito.run" rel="noopener noreferrer" target="_blank">mecaota</a>の卒業研究にて開発を行った、スマートコントラクトを用いたライドシェアシステムのサイトです。</p>
-                <a href="https://github.com/mecaota/rideshare-eth-front" rel="noopener noreferrer" target="_blank" className="button is-outlined is-black">
+                <a href="https://github.com/mecaota/rideshare-eth-front" rel="noopener noreferrer" target="_blank" className="button is-outlined is-dark">
                     <FontAwesomeIcon icon={['fab', 'github']} size="1x" />
                     GitHub
                 </a>
